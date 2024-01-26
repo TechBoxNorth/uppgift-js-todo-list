893d832bdc9589a2cb146012fe49fdf02fa716dd
TechBoxNorth <techboxnorth@gmail.com>
2024-01-26 18:46:10 +0100

    tasks can now be added to the list

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let tasks = [];
-function newTask(task){
-    const description = task;
-    const done = false;
-    return {
-        description,
-        done
-    }
-}
-
-tasks.push(newTask('Make dinner'));
-tasks.push(newTask('Make dinner'));
-tasks.push(newTask('Make dinner'));
-
-console.log(tasks);
+const taskList = document.querySelector('.task-list');
+const createBtn = document.querySelector('.create-btn');
+createBtn.addEventListener('click', () => {
+    const taskTxt = document.querySelector('.task-text');
+    taskList.insertAdjacentHTML("afterbegin", `<li class="task">
+    <p>${taskTxt.value}</p>
+    <div class="icons">
+        <img src="img/arrow_up.svg" class="move-up" alt="up arrow">
+        <img src="img/arrow_down.svg" class="move-down" alt="down arrow">
+        <img src="img/delete.svg" class="delete-task" alt="trash bin">
+        <img src="img/check.svg" class="check-task" alt="check mark">
+    </div>
+</li>`);
+    taskTxt.value = null;
+});
